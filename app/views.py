deb9936782ac5d4cd8001754308ed3599acfc558
paulhenr <voxskynned@gmail.com>
2024-12-20 10:04:38 -0300

    added basic configuration for routes

--- app/views.py
+++ app/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Cadete
 
 # Create your views here.
 def list_cadete(request):
 	cadetes = Cadete.objects.all()
 	return render(request, 'index.html', {'cadetes': cadetes})
+
+def index(request):
+	return render(request, 'app/index.html')
